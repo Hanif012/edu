96443e623c55823ad9f71cbb086805a5b918e21b
Hanif012 <andque12345012@gmail.com>
2024-03-05 11:24:03 +0700

    project cs init

---
+++ eduOOGP/Program.cs
@@ -0,0 +1,30 @@
+﻿public abstract class Shape
+{
+    public abstract double CalculateArea();
+}
+
+public class Circle : Shape
+{
+    private double radius;
+
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+
+    public override double CalculateArea()
+    {
+        return Math.PI * radius * radius;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        float radius = float.Parse(Console.ReadLine());
+        Circle circle = new Circle(radius);
+        double area = circle.CalculateArea();
+        Console.WriteLine("Area of the circle: " + area);
+    }
+}
